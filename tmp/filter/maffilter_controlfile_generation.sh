eba5db427cedfb3b8500083d346fafa6f6be6233
Iker <54534843+rivasiker@users.noreply.github.com>
2020-04-10 11:18:14 +0200

    Update maffilter_controlfile_generation.sh

--- tmp/filter/maffilter_controlfile_generation.sh
+++ tmp/filter/maffilter_controlfile_generation.sh
@@ -13,5 +13,11 @@ echo "input.file=$1" > ../control_file
 echo "input.file.compression=gzip" >> ../control_file
 echo "input.format=Maf" >> ../control_file
 echo "output.log=../maf_filtering.log" >> ../control_file
-echo -e "maf.filter=Subset(species=($2, $3, $4, $5), strict=yes, keep=no, remove_duplicates=yes), XFullGap(species=($2, $3, $4, $5)), Merge(species=($2, $3, $4, $5)), MaskFilter(species=($2, $3, $4, $5), window.size=100, window.step=1, max.masked=80), Merge(species=($2, $3, $4, $5), dist_max=100, rename_chimeric_chromosomes=yes), MinBlockLength(min_length=500), SequenceStatistics(statistics=(BlockLength,AlnScore,BlockCounts),ref_species=$2,file=../data.statistics.csv,compression=none), Output(file=../filtered.maf, compression=none, mask=yes)" >> ../control_file
-
+echo "maf.filter=Subset(species=($2, $3, $4, $5), strict=yes, keep=no, remove_duplicates=yes), \\" >> ../tmp/control_file
+echo "XFullGap(species=($2, $3, $4, $5)), \\" >> ../tmp/control_file
+echo "Merge(species=($2, $3, $4, $5)), \\" >> ../tmp/control_file
+echo "AlnFilter(species=($2, $3, $4, $5), window.size=500, window.step=500, max.gap=0.2, max.ent=0.2, missing_as_gap=yes, relative=yes, file=none), \\" >> ../tmp/control_file  
+echo "MaskFilter(species=($2, $3, $4, $5), window.size=10, window.step=1, max.masked=2), \\" >> ../tmp/control_file
+echo "Merge(species=($2, $3, $4, $5), dist_max=100, rename_chimeric_chromosomes=yes), \\" >> ../tmp/control_file
+echo "MinBlockLength(min_length=500), \\" >> ../tmp/control_file
+echo "Output(file=../tmp/filtered.maf, compression=none, mask=yes)" >> ../tmp/control_file
