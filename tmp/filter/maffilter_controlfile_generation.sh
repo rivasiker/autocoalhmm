8c784fd61f6eeb459e087efdf4afb793b336c79a
Iker <54534843+rivasiker@users.noreply.github.com>
2020-04-14 09:48:58 +0200

    Update paths

--- tmp/filter/maffilter_controlfile_generation.sh
+++ tmp/filter/maffilter_controlfile_generation.sh
@@ -13,11 +13,11 @@ echo "input.file=$1" > ../control_file
 echo "input.file.compression=gzip" >> ../control_file
 echo "input.format=Maf" >> ../control_file
 echo "output.log=../maf_filtering.log" >> ../control_file
-echo "maf.filter=Subset(species=($2, $3, $4, $5), strict=yes, keep=no, remove_duplicates=yes), \\" >> ../tmp/control_file
-echo "XFullGap(species=($2, $3, $4, $5)), \\" >> ../tmp/control_file
-echo "Merge(species=($2, $3, $4, $5)), \\" >> ../tmp/control_file
-echo "AlnFilter(species=($2, $3, $4, $5), window.size=500, window.step=500, max.gap=0.2, max.ent=0.2, missing_as_gap=yes, relative=yes, file=none), \\" >> ../tmp/control_file  
-echo "MaskFilter(species=($2, $3, $4, $5), window.size=10, window.step=1, max.masked=2), \\" >> ../tmp/control_file
-echo "Merge(species=($2, $3, $4, $5), dist_max=100, rename_chimeric_chromosomes=yes), \\" >> ../tmp/control_file
-echo "MinBlockLength(min_length=500), \\" >> ../tmp/control_file
-echo "Output(file=../tmp/filtered.maf, compression=none, mask=yes)" >> ../tmp/control_file
+echo "maf.filter=Subset(species=($2, $3, $4, $5), strict=yes, keep=no, remove_duplicates=yes), \\" >> ../control_file
+echo "XFullGap(species=($2, $3, $4, $5)), \\" >> ../control_file
+echo "Merge(species=($2, $3, $4, $5)), \\" >> ../control_file
+echo "AlnFilter(species=($2, $3, $4, $5), window.size=500, window.step=500, max.gap=0.2, max.ent=0.2, missing_as_gap=yes, relative=yes, file=none), \\" >> ../control_file  
+echo "MaskFilter(species=($2, $3, $4, $5), window.size=10, window.step=1, max.masked=2), \\" >> ../control_file
+echo "Merge(species=($2, $3, $4, $5), dist_max=100, rename_chimeric_chromosomes=yes), \\" >> ../control_file
+echo "MinBlockLength(min_length=500), \\" >> ../control_file
+echo "Output(file=../filtered.maf, compression=none, mask=yes)" >> ../control_file
