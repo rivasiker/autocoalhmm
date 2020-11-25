6d5ceee7f2f08430ffcc16baa868bf96524db4ac
Iker <54534843+rivasiker@users.noreply.github.com>
2020-11-25 08:53:52 +0100

    ifelse to elif

--- tmp/coalhmm/workflow.py
+++ tmp/coalhmm/workflow.py
@@ -11,10 +11,10 @@ gwf = Workflow()
 if len(pickle.load(open('../params.pickle', 'rb'))) == 7:
     with open('../params.pickle', 'rb') as f:
         [path, species1, species2, species3, species4, target_seqname, big_maf_file] = pickle.load(f)
-ifelse len(pickle.load(open('../params.pickle', 'rb'))) == 8:
+elif len(pickle.load(open('../params.pickle', 'rb'))) == 8:
     with open('../params.pickle', 'rb') as f:
         [path, species1, species2, species3, species4, target_seqname, big_maf_file, error_sp1] = pickle.load(f)
-ifelse len(pickle.load(open('../params.pickle', 'rb'))) == 9:
+elif len(pickle.load(open('../params.pickle', 'rb'))) == 9:
     with open('../params.pickle', 'rb') as f:
         [path, species1, species2, species3, species4, target_seqname, big_maf_file, error_sp1, error_sp2] = pickle.load(f)
 
